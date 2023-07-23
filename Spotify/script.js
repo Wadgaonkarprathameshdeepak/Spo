42016dfa4f21db836f1e7a02a6e993201730551e
Prathamesh Wadgaonkar <wadgaonkarprathamesh@gmail.com>
2023-07-23 21:38:58 +0530

    first commit

---
+++ Spotify/script.js
@@ -0,0 +1,27 @@
+var audio = new Audio("song/9.mp3");
+
+var playButton = document.querySelector(".play .fa-play");
+var pauseButton = document.querySelector(".play .fa-pause");
+
+playButton.addEventListener("click", function() {
+  audio.play();
+});
+
+pauseButton.addEventListener("click", function() {
+  audio.pause();
+});
+
+var audio1 = new Audio("song/7.mp3");
+
+var playButton1 = document.querySelector1(".play .fa-play");
+var pauseButton1 = document.querySelector1(".play .fa-pause");
+
+playButton1.addEventListener1("click", function() {
+  audio1.play();
+});
+
+pauseButton1.addEventListener1("click", function() {
+  audio1.pause();
+});
+
+
